562173368efc4985a9cffdaf5402ce9e3b21da24
Zayd Mohammed <zaydiscool777@gmail.com>
2025-09-16 07:12:34 -0400

    Added Rand.hs.

--- me/sort.hs
+++ me/sort.hs
@@ -1,7 +1,5 @@
-module Main where
 import Text.Read
-
-input = [7,1,8,2,6,9,4,2,5,8,0,2,6,8,2,3,6,3,5,8]
+import MeRand
 
 sort :: Ord a => [a] -> [a] -- divide
 sort [] = []
@@ -24,12 +22,19 @@ sort2 (a:sa) (b:sb)
   | a < b = a:(sort2 sa (b:sb))
   | otherwise = b:(sort2 (a:sa) sb)
 
--- main = putStrLn $ show $ sort input
-readInts :: IO [Int] = do
+{-
+input = [7,1,8,2,6,9,4,2,5,8,0,2,6,8,2,3,6,3,5,8]
+main = putStrLn $ show $ sort input
+--}
+
+--{-
+readVals :: Read a => IO [a] -- type annotation by ai
+readVals = do
   x <- getLine
-  let y = readMaybe x :: Maybe Int
+  let y = readMaybe x
   case y of
     Nothing -> return []
-    Just n -> readInts >>= return . (n:)
+    Just n -> readVals >>= return . (n:)
 
-main = readInts >>= (putStrLn . show . sort)
+main = (readVals :: IO [Int]) >>= putStrLn . show . sort
+--}
